9d312ab1a322ab95f5f00b8d2cc61a936e8a71b7
hughbagan <hbagan@ualberta.ca>
2024-01-09 14:13:46 -0700

    WIP spawning

--- Scenes/Level.cs
+++ Scenes/Level.cs
@@ -4,28 +4,45 @@ using System.Drawing;
 
 public class Level : Node2D
 {
-    private float width;
-    private float height;
+    private PackedScene enemyScene;
+    private int width;
+    private int height;
     private Timer spawnTimer;
 
     public override void _Ready()
     {
-        width = GetViewport().Size.x;
-        height = GetViewport().Size.y;
+        enemyScene = GD.Load<PackedScene>("res://Scenes/Enemy/Enemy.tscn");
+        width = (int) GetViewport().Size.x;
+        height = (int) GetViewport().Size.y;
         spawnTimer = GetNode<Timer>("SpawnTimer");
         spawnTimer.Connect("timeout", this, "_on_SpawnTimer_timeout");
         spawnTimer.Start();
+        GD.Randomize();
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
 
     public void _on_SpawnTimer_timeout()
     {
-        // get random 0 - viewport width and height
-        // then convert to global position and instance an Enemy
+        for (int i=0; i<3; i++)
+        {
+            Enemy enemyInstance = (Enemy) enemyScene.Instance();
+            int spawnX;
+            int spawnY;
+            if (GD.Randf() > 0.5)
+            {
+                spawnX = (int) GD.RandRange(32, width-32);
+                spawnY = GD.Randf() > 0.5 ? -32 : height+32;
+            }
+            else
+            {
+                spawnX = GD.Randf() > 0.5 ? -32 : width+32;
+                spawnY = (int) GD.RandRange(32, height-32);
+            }
+            Vector2 viewportTransform = GetViewportTransform().origin;
+            // enemyInstance.GlobalPosition = new Vector2(
+            //     viewportTransform.x + spawnX,
+            //     viewportTransform.y + spawnY);
+            GD.Print(GlobalPosition, enemyInstance.GlobalPosition, GetViewportTransform());
+            GetNode<YSort>("/root/Level/Enemies").AddChild(enemyInstance);
+        }
     }
 }
